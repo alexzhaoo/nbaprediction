0245bdce9db91df5672b34ceb1454ad7b38d0256
Alexander Zhao <zhaotianxiao722@icloud.com>
2024-05-17 16:52:10 -0400

    finished data collection and cleaning

--- dataclean/sortingallStats.py
+++ dataclean/sortingallStats.py
@@ -1,12 +1,13 @@
 import pandas as pd
 import numpy as np
 
-filename = 'allStats.csv'
+filename = 'datasets/allStats.csv'
 
 file = pd.read_csv(filename)
 
 
-file.drop(['TEAM_ABBREVIATION', 'TEAM_ID', 'PLAYER_ID', 'SEASON_ID', 'LEAGUE_ID' ], axis=1, inplace=True)
+file.drop(['TEAM_ABBREVIATION', 'TEAM_ID', 'PLAYER_ID', 'SEASON_ID', 'LEAGUE_ID', 'FullName' , 'PLAYER_AGE' ], axis=1, inplace=True)
 
-file['PTS'] = file['PTS'] / file['GP']
+
+file.to_csv("allStatsFiltered.csv", index = False)
 
