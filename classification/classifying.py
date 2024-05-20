a8016c8c142496f93ac5989bbdd4af6489cbe7a0
Alexander Zhao <zhaotianxiao722@icloud.com>
2024-05-20 16:09:55 -0400

    perceptron has an 81% accuracy of guessing all stars from rookie first two rookie seasons

--- classification/classifying.py
+++ classification/classifying.py
@@ -4,49 +4,49 @@ from sklearn.preprocessing import StandardScaler
 import numpy as np
 from sklearn.impute import SimpleImputer
 
+
 def accuracies(Xmat, yvec):
+
     onescolumn = np.ones((Xmat.shape[0], 1))
-    
     Xmat = np.concatenate((Xmat, onescolumn), axis=1)
+
     eta = 0.001
 
-    weightVector  = perceptronLearningRule(Xmat, yvec , eta)
+    weightVector = perceptronLearningRule(Xmat, yvec, eta)
+
+
+    weightVector = weightVector.ravel()
+
 
     weightVector = weightVector / np.linalg.norm(weightVector)
 
-
-    samples , features = Xmat.shape
-
     zvec = np.dot(Xmat, weightVector)
-
     perceptronPred = zvec >= 0
 
-    accuracy = np.sum(perceptronPred == yvec)/ len(yvec)
+    accuracy = np.sum(perceptronPred == yvec.flatten()) / len(yvec)
 
     return accuracy
 
 
-def perceptronLearnijngRule(Xaug, yvec, eta=1):
-    imax = 10000
-    estweights = np.ones((Xaug.shape[1], 1))
+def perceptronLearningRule(Xaug, yvec, eta=1):
+    imax = 15000
+    samples, features = Xaug.shape
+    weights = np.ones(features).reshape(-1, 1)
 
-    for i_used in range(imax + 1):
+    for _ in range(imax):
         missed = 0
-        weights = estweights
-        rvec = np.zeros_like(yvec)
 
-
-        scores = np.dot(Xaug, estweights)
+        scores = np.dot(Xaug, weights)
 
         qvec = scores >= 0
-        rvec = yvec - qvec
-        estweights = estweights + eta * np.dot(Xaug.T, rvec)
 
+        rvec = yvec - qvec.flatten()
 
+        weights = weights + eta * np.dot(Xaug.T, rvec).reshape(-1, 1)
+        
         missed = np.linalg.norm(rvec, 1) > 0
 
         if not missed:
-            weights = estweights
             break
 
     return weights
